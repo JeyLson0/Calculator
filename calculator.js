beaceca4c4327c7446a66edffb051ad100058465
Jeylson0 <jhonalson.lugada@gmail.com>
2023-07-12 13:57:27 +0800

    version 1.0.3 decimal point, back and clear button
    When entering a number the period button can create a floating point
    number. Back button removes one value from the arr containing all the
    numbers. Clear button removes all data from the arr, creating an
    empty arr.

--- calculator.js
+++ calculator.js
@@ -1,45 +1,38 @@
-function addition(a,b) {
-    let c = a+b;
-    return c;
+function evaluate() {
+    function addition(a,b) {
+        let c = a+b;
+        return c;
+    }
+    
+    function subtract(a,b) {
+        let c = a-b;
+        return c;
+    }
+    
+    function multiplication(a,b) {
+        let c = a*b;
+        return c;
+    }
+    
+    function division (a,b) {
+        let c = a/b;
+        return c;
+    }
+    
 }
 
-function subtract(a,b) {
-    let c = a-b;
-    return c;
-}
-
-function multiplication(a,b) {
-    let c = a*b;
-    return c;
-}
-
-function division (a,b) {
-    let c = a/b;
-    return c;
-}
 
 ///////////////////////////////////////////////////////
 
+//Animation
 let numpad = document.querySelectorAll(".keypad");
-let screenCurrentExpression = document.querySelector('#current_expression')
-let arr = []
-let currentExpression;
-
-
-
 
 numpad.forEach(element => {
     element.addEventListener('mouseover', (e) => {
         element.setAttribute('style','font-size: 40px;')
     })
     element.addEventListener('mousedown', (e) => {
-        //console.log(e.target.value);
         element.setAttribute('style','font-size: 50px;')
-        console.log(e.target.value)
-        let val = e.target.value
-        arr.push(val);
-        currentExpression = arr.join('');
-        screenCurrentExpression.textContent = currentExpression;
     })
     element.addEventListener('mouseup', (e) => {
         element.setAttribute('style','font-size: 40px;')
@@ -50,6 +43,63 @@ numpad.forEach(element => {
 });
 
 
+//Numbers
+let screenCurrentExpression = document.querySelector('#current_expression')
+let arr = []
+let currentExpression;
 
 
 
+
+let numbers = document.querySelectorAll('.number');
+numbers.forEach(element => {
+    element.addEventListener('mousedown', (e) => {
+        let val = e.target.value
+        arr.push(val);
+        currentExpression = Number(arr.join(''));
+        console.log(arr);
+        screenCurrentExpression.textContent = currentExpression;
+    })
+})
+
+//Decimal period
+let decimal = document.querySelector('#period');
+
+function removeDecimal(e) {
+    arr.push(e.target.value)
+    currentExpression = arr.join('');
+    screenCurrentExpression.textContent= currentExpression;
+    decimal.removeEventListener('mousedown', removeDecimal);
+}
+decimal.addEventListener('mousedown', removeDecimal);
+
+
+//C or backspace
+let remove = document.querySelector('#backspace')
+remove.addEventListener('mousedown', (e) => {
+    let removed = arr.pop()
+    if (removed == '.'){
+        decimal.addEventListener('mousedown', removeDecimal);
+    }
+    console.log(removed)
+
+    currentExpression = Number(arr.join(''));
+    screenCurrentExpression.textContent = currentExpression;
+});
+
+// AC or clear
+
+let clear = document.querySelector('#ac')
+clear.addEventListener('mousedown', (e) => {
+    arr.splice(0, arr.length)
+    currentExpression = Number(arr.join(''));
+    decimal.addEventListener('mousedown', removeDecimal);
+    screenCurrentExpression.textContent = currentExpression;
+})
+
+
+// operations
+let add = document.querySelector('#add')
+add.addEventListener('mousedown', (e) => {
+    console.log('plus')
+})
