c586cd9173ee6f86bfcc5a51feddad0cf7f4a77a
Jeylson0 <jhonalson.lugada@gmail.com>
2023-07-13 21:52:31 +0800

    version 1.0.4 Operator feature
    Operators operate their respective function.
    
    Each operator uses rest parameters to pass values from
    the 'newArr' to the operator function.

--- calculator.js
+++ calculator.js
@@ -1,25 +1,5 @@
-function evaluate() {
-    function addition(a,b) {
-        let c = a+b;
-        return c;
-    }
-    
-    function subtract(a,b) {
-        let c = a-b;
-        return c;
-    }
-    
-    function multiplication(a,b) {
-        let c = a*b;
-        return c;
-    }
-    
-    function division (a,b) {
-        let c = a/b;
-        return c;
-    }
-    
-}
+
+
 
 
 ///////////////////////////////////////////////////////
@@ -46,10 +26,7 @@ numpad.forEach(element => {
 //Numbers
 let screenCurrentExpression = document.querySelector('#current_expression')
 let arr = []
-let currentExpression;
-
-
-
+let currentExpression = 0;
 
 let numbers = document.querySelectorAll('.number');
 numbers.forEach(element => {
@@ -57,8 +34,15 @@ numbers.forEach(element => {
         let val = e.target.value
         arr.push(val);
         currentExpression = Number(arr.join(''));
-        console.log(arr);
-        screenCurrentExpression.textContent = currentExpression;
+        if (sign == 0) {
+            screenCurrentExpression.textContent = currentExpression;
+            console.log(currentExpression)
+        } 
+        if (sign == 1) {
+            currentExpression *= -1;
+            screenCurrentExpression.textContent = currentExpression;
+            console.log(currentExpression)
+        }
     })
 })
 
@@ -82,24 +66,187 @@ remove.addEventListener('mousedown', (e) => {
         decimal.addEventListener('mousedown', removeDecimal);
     }
     console.log(removed)
-
     currentExpression = Number(arr.join(''));
-    screenCurrentExpression.textContent = currentExpression;
+    if (sign == 0) {
+        screenCurrentExpression.textContent = currentExpression;
+        console.log(currentExpression)
+    } 
+    if (sign == 1) {
+        currentExpression *= -1;
+        screenCurrentExpression.textContent = currentExpression;
+        console.log(currentExpression)
+    }
 });
 
 // AC or clear
-
-let clear = document.querySelector('#ac')
+let clear = document.querySelector('#ac');
 clear.addEventListener('mousedown', (e) => {
     arr.splice(0, arr.length)
     currentExpression = Number(arr.join(''));
     decimal.addEventListener('mousedown', removeDecimal);
+    sign = 0; 
     screenCurrentExpression.textContent = currentExpression;
 })
 
+//Negative/Positive : where positive is 0 and negative is 1
+let sign = 0; //positive
+
+let numSign = document.querySelector('#integer')
+numSign.addEventListener('mousedown', (e) => {
+    if (sign == 0) {
+        sign = 1;
+        console.log('negative')
+        currentExpression *= -1
+        console.log(currentExpression)
+        return screenCurrentExpression.textContent = currentExpression;
+    };
+    if (sign == 1) {
+        sign = 0;
+        console.log('positive')
+        currentExpression *= -1
+        console.log(currentExpression)
+        return screenCurrentExpression.textContent = currentExpression;
+    }
+})
 
 // operations
-let add = document.querySelector('#add')
-add.addEventListener('mousedown', (e) => {
-    console.log('plus')
+/* function addition(a,b) {
+    currentExpression = a+b;
+    return screenCurrentExpression.textContent = currentExpression;
+}
+
+function subtract(a,b) {
+    let c = a-b;
+    return c;
+}
+
+function multiplication(a,b) {
+    let c = a*b;
+    return c;
+}
+
+function division (a,b) {
+    let c = a/b;
+    return c;
+} */
+
+let addition = document.querySelector('#add');
+let division = document.querySelector('#divide');
+let multiplication = document.querySelector('#multiply');
+let subtraction = document.querySelector('#subtract');
+let previousValue = document.querySelector('#previousValue')
+let num = 0;
+let newArr = []
+
+addition.addEventListener('mousedown', (e) => {
+    num = currentExpression;
+    newArr.push(num);
+    console.log(`newArr = ${newArr}`)
+    if (newArr.length == 1 ) {
+        previousValue.textContent = `${num}+`
+        arr.splice(0, arr.length)
+        currentExpression = Number(arr.join(''));
+        return screenCurrentExpression.textContent = currentExpression;
+    }
+    if (newArr.length == 2 ) {
+        sum(...newArr)
+    }
+
 })
+
+function sum(a, b){
+    result = a + b
+    newArr.splice(0, newArr.length)
+    newArr.push(result);
+    console.log(newArr)
+    previousValue.textContent = `${result}+`;
+    arr.splice(0, arr.length)
+    currentExpression = Number(arr.join(''));
+    return screenCurrentExpression.textContent = currentExpression;
+}
+
+
+division.addEventListener('mousedown', (e) => {
+    num = currentExpression;
+    newArr.push(num);
+    console.log(`newArr = ${newArr}`)
+    if (newArr.length == 1 ) {
+        previousValue.textContent = `${num}/`
+        arr.splice(0, arr.length)
+        currentExpression = Number(arr.join(''));
+        return screenCurrentExpression.textContent = currentExpression;
+    }
+    if (newArr.length == 2 ) {
+        quotient(...newArr)
+    }
+
+})
+
+function quotient(a, b){
+    result = a / b
+    newArr.splice(0, newArr.length)
+    newArr.push(result);
+    console.log(newArr)
+    previousValue.textContent = `${result}/`;
+    arr.splice(0, arr.length)
+    currentExpression = Number(arr.join(''));
+    return screenCurrentExpression.textContent = currentExpression;
+}
+
+multiplication.addEventListener('mousedown', (e) => {
+    num = currentExpression;
+    newArr.push(num);
+    console.log(`newArr = ${newArr}`)
+    if (newArr.length == 1 ) {
+        previousValue.textContent = `${num}x`
+        arr.splice(0, arr.length)
+        currentExpression = Number(arr.join(''));
+        return screenCurrentExpression.textContent = currentExpression;
+    }
+    if (newArr.length == 2 ) {
+        quotient(...newArr)
+    }
+
+})
+
+function product(a, b){
+    result = a * b
+    newArr.splice(0, newArr.length)
+    newArr.push(result);
+    console.log(newArr)
+    previousValue.textContent = `${result}x`;
+    arr.splice(0, arr.length)
+    currentExpression = Number(arr.join(''));
+    return screenCurrentExpression.textContent = currentExpression;
+}
+
+
+subtraction.addEventListener('mousedown', (e) => {
+    num = currentExpression;
+    newArr.push(num);
+    console.log(`newArr = ${newArr}`)
+    if (newArr.length == 1 ) {
+        previousValue.textContent = `${num}-`
+        arr.splice(0, arr.length)
+        currentExpression = Number(arr.join(''));
+        return screenCurrentExpression.textContent = currentExpression;
+    }
+    if (newArr.length == 2 ) {
+        difference(...newArr)
+    }
+
+})
+
+function difference(a, b){
+    result = a - b
+    newArr.splice(0, newArr.length)
+    newArr.push(result);
+    console.log(newArr)
+    previousValue.textContent = `${result}-`;
+    arr.splice(0, arr.length)
+    currentExpression = Number(arr.join(''));
+    return screenCurrentExpression.textContent = currentExpression;
+}
+
+//Evaluate
+
